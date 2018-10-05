8e7e0dc20f4b87cb14949a4dbc39eb069b74b415
developer-kikikaikai <kikikaikaikai@gmail.com>
2018-10-06 08:54:25 +0900

    <factory_method> organize main

--- factory_method/sample/main.c
+++ factory_method/sample/main.c
@@ -1,20 +1,21 @@
 #include <stdlib.h>
 #include "factory.h"
 
-int main() {
-	Factory factory;
-	Product product;
-
-	factory = toyota_factory_new();
-	product = factory->product_factory();
+static void do_product(Factory factory) {
+	Product product = factory->product_factory();
 	product->show_name(product);
 	factory->product_free(product);
+}
+
+int main() {
+	Factory factory;
+
+	factory = toyota_factory_new();
+	do_product(factory);
 	free(factory);
 
 	factory = nissan_factory_new();
-	product = factory->product_factory();
-	product->show_name(product);
-	factory->product_free(product);
+	do_product(factory);
 	free(factory);
 	return 0;
 }
